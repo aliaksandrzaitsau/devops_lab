c420a74446e483da5e7319e06452fbd77b2fae48
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-03-13 14:53:18 +0200

    Update H2_Task3.py

--- H2_Task3.py
+++ H2_Task3.py
@@ -1,20 +1,25 @@
 """homework"""
+# pylint: disable=invalid-name
 print("check your first number:")
 X = (int(input()))
 print("check your second number:")
 Y = (int(input()))
+
+
 def self_numbers(left, right):
     """write here"""
-    def self_dividing(n):
+    def self_dividing(N):
         """something"""
-        for a in str(n):
-            if a == '0' or n % int(a) > 0:
+        for A in str(N):
+            if A == '0' or N % int(A) > 0:
                 return False
         return True
     ans = []
-    for n in range(left, right + 1):
-        if self_dividing(n):
-            ans.append(n)
+    for N in range(left, right + 1):
+        if self_dividing(N):
+            ans.append(N)
     return ans
+
+
 print("self dividing numbers are:")
 print(self_numbers(X, Y))
