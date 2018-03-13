4a16e249a0de08da8c90cff6817d5c5ad58cb6fe
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-03-13 14:49:13 +0200

    Update H2_Task1.py

--- H2_Task1.py
+++ H2_Task1.py
@@ -1,6 +1,13 @@
 """hometask"""
+# pylint: disable=invalid-name
 X = int(input())
 Y = int(input())
 F = int(input())
 N = int(input())
-print([[i, j, k] for i in range(X + 1) for j in range(Y + 1) for k in range(F + 1) if ((i + j + k) != N)])
+print([
+    [i, j, k]
+    for i in range(X + 1)
+    for j in range(Y + 1)
+    for k in range(F + 1)
+    if ((i + j + k) != N)
+    ])
