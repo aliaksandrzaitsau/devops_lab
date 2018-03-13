1f46c593879b1d16cac15e577a8a33ae8551a3b7
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-03-13 10:31:21 +0200

    Update H2_Task1.py

--- H2_Task1.py
+++ H2_Task1.py
@@ -1,7 +1,6 @@
-
-
-x=int (input()) 
-y=int (input())
-f=int (input())
-n=int (input()) 
-print ([[ i, j, k ] for i in range( x + 1) for j in range( y + 1) for k in range( f + 1) if ( ( i + j + k ) != n )]) 
+"""hometask"""
+X = int(input())
+Y = int(input())
+F = int(input())
+N = int(input())
+print([[i, j, k] for i in range(X + 1) for j in range(Y + 1) for k in range(F + 1) if ((i + j + k) != N)])
