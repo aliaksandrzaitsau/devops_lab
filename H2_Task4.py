477527a6dd49ab3f79e89d4831773fe31e0a21fc
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-03-13 10:48:01 +0200

    Update H2_Task4.py

--- H2_Task4.py
+++ H2_Task4.py
@@ -1,11 +1,9 @@
-
-
+"""homework"""
 from collections import OrderedDict
-
-words = OrderedDict()
-a = int(input())
-for _ in range(a):
-	key = input()
-	words[key] = words.get(key, 0) + 1
-print (len(words))
-print (*[str(words[key]) for key in words], sep = " ")
+WORDS = OrderedDict()
+A = int(input())
+for _ in range(A):
+    key = input()
+    WORDS[key] = WORDS.get(key, 0) + 1
+print(len(WORDS))
+print(*[str(WORDS[key]) for key in WORDS], sep="")
